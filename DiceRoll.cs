e7112a7c43da57d938a3d3cc8d2d6b9e9d43d2d4
Gmurd35 <119529326+Gmurd35@users.noreply.github.com>
2024-01-26 12:35:39 -0700

    Add files via upload

---
+++ DiceRoll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    using System;
+
+    class DiceRoll
+    {
+        // import random thing
+        private Random random = new Random();
+        // Create the array
+        public int[] SimulateRolls(int numRolls)
+        {
+            int[] results = new int[13];
+            // Roll the dice
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+                int diceTotal = die1 + die2;
+
+                results[diceTotal]++;
+            }
+
+            return results;
+        }
+    }
+}
+
---
+++ Mission2AssignmentProject.cs
@@ -0,0 +1,30 @@
+﻿//Grant Murdock
+using Mission2Assignment;
+using System;
+//Make the class
+class DiceSimulator
+{
+    static void Main()
+    {
+        // Figure out how many rolls the user wants
+        Console.Write("How many dice rolls would you like to simulate? ");
+        if (int.TryParse(Console.ReadLine(), out int numRolls) && numRolls > 0)
+        {
+            // Create an instance of the DiceRoll class and simulate dice rolls
+            DiceRoll roller = new DiceRoll();
+            int[] results = roller.SimulateRolls(numRolls);
+        // Figure out how many times each combination was rolled
+            for (int i = 2; i <= 12; i++)
+            {
+                int percentage = (int)Math.Round((double)results[i] / numRolls * 100);
+                Console.WriteLine($"{i}: {new string('*', percentage)}");
+            }
+
+            Console.WriteLine($"\nTotal number of rolls = {numRolls}.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number greater than 0.");
+        }
+    }
+}
